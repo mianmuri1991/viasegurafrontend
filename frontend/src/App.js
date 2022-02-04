3349b883e9b727e5addb0fe58fd05ad8bfdb9510
Christopher <cvaccaro@espol.edu.ec>
2022-02-04 03:26:11 -0500

    Division de componentes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,14 @@ function App() {
   return (
     <div className="App">
       <Cabecera />
+      <div className='container-fluid'>
+        <div className="row row-cols-2">
+          <div className="col-4">Column</div>
+          <div className="col-8">Column</div>
+          <div className="col-4">Column</div>
+          <div className="col-8">Column</div>
+        </div>
+      </div>
     </div>
   );
 }
