dd8a127c094b1adef9eb34525fcbe71ee8851f28
Christopher <cvaccaro@espol.edu.ec>
2022-02-07 23:10:09 -0500

    Tabla plantilla agregada

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
 import Cabecera from './components/Cabecera'
+import Mapa from './components/Mapa';
+import Tabla from './components/Tabla';
 
 function App() {
   return (
     <div className="App">
       <Cabecera />
-      <div className='container-fluid'>
-        <div className="row row-cols-2">
-          <div className="col-4">Column</div>
-          <div className="col-8">Column</div>
-          <div className="col-4">Column</div>
-          <div className="col-8">Column</div>
+      <div className='container-fluid' >
+        <div className="row row-cols-2" style={{height: '80vh'}}>
+          <div className="col-4 p-3">Column</div>
+          <div className="col-8 p-3 "><Mapa/></div>
+          <div className="col-4 p-3">Column</div>
+          <div className="col-8 p-3 "><Tabla/></div>
         </div>
       </div>
     </div>
--- frontend/src/components/Cabecera.js
+++ frontend/src/components/Cabecera.js
@@ -2,13 +2,11 @@ import React, { Component } from 'react';
 
 export default class Cabecera extends Component {
     render() {
-        return <nav className="navbar navbar-dark ">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="/">
-                    <img src="/logo.png" alt="" height="100"/>
-                </a>
+        return (<nav className="navbar navbar-dark " >
+            <div className="container-fluid" >
+                <img src="/logo.png" alt="" style={{ maxHeight: '5rem' }}/>
                 <span className='fs-1 text-light'>Siniestros en las principales vías de Guayaquil</span>
             </div>
-        </nav>;
+        </nav>);
     }
 }
